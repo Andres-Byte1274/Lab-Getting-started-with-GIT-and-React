d99067a5ee1fb3247001bcf426e726c1c9413782
andresMen1274 <andresmendez1274@gmail.com>
2025-10-19 20:33:33 -0700

    linking backend

---
+++ packages/express-backend/user-services.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import userModel from "./user.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+function getUsers(name, job) {
+  let promise;
+
+  if (name && job) {
+    // 🔹 both filters present
+    promise = findUserByNameAndJob(name, job);
+  } else if (name && !job) {
+    promise = findUserByName(name);
+  } else if (job && !name) {
+    promise = findUserByJob(job);
+  } else {
+    promise = userModel.find(); // all users
+  }
+
+  return promise;
+}
+
+function findUserById(id) {
+  return userModel.findById(id);
+}
+
+function addUser(user) {
+  const userToAdd = new userModel(user);
+  const promise = userToAdd.save();
+  return promise;
+}
+
+function findUserByNameAndJob(name, job) {
+  return userModel.find({ name: name, job: job });
+}
+
+function findUserByName(name) {
+  return userModel.find({ name: name });
+}
+
+function findUserByJob(job) {
+  return userModel.find({ job: job });
+}
+
+function deleteUserById(id) {
+  return userModel.findByIdAndDelete(id);
+}
+
+export default {
+  addUser,
+  getUsers,
+  findUserById,
+  findUserByName,
+  findUserByJob,
+  findUserByNameAndJob,
+  deleteUserById,
+};
